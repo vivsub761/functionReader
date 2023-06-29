73b4e8c8398fdd9be9098b6e71d081e2422cd801
Vivek Subramanyan <vivsub761@berkeley.edu>
2023-06-29 11:07:18 -0700

    init commit

---
+++ libraryDocumentation/README.md
@@ -0,0 +1,6 @@
+To run the file, run the updateDocumentation file on command line with the flag -f "path_to_file_with_functions", e.g. python3 updateDocumentation.py -f ./functions.py
+
+IMPORTANT: For each function in the file in question:
+    There must be at least one docstring in the function. The very first docstring MUST contain a brief description of the function. Subsequent ones do not matter to this script.
+    The function must have argument annotation(s), e.g. def function(x: int, y: str)
+    The function must have output annotation(s), e.g. def function(x: int, y: str) -> Tuple[int, str]
---
+++ libraryDocumentation/updateDocumentation.py
@@ -0,0 +1,75 @@
+import re
+import json
+import ast
+import argparse
+
+
+def astUpdate(filepath):
+    functionDict = {}
+
+    with open(filepath, "rb") as f:
+        file_data = f.read()
+    astTree = ast.parse(file_data, filepath)
+    for node in ast.walk(astTree):
+        if isinstance(node, ast.FunctionDef):
+            funcName = node.name
+            functionDict[funcName] = {}
+            docstring = ast.get_docstring(node).strip().replace("\n", " ").split()
+            functionDict[funcName]["Description"] = " ".join(docstring)
+            functionDict[funcName]["Input(s)"] = getExpectedArgs(node)
+            functionDict[funcName]["Output(s)"] = getExpectedOutputs(node)
+    
+            
+    return functionDict
+
+def getExpectedOutputs(astNode):
+    def returnOutputName(element):
+        if isinstance(element, ast.Name):
+            return element.id
+        elif isinstance(element, ast.Constant):
+            return element.value
+        return ""
+    outputs = {}
+    outputNames = []
+    if isinstance(astNode.body[-1].value, ast.Tuple):
+        for element in astNode.body[-1].value.elts:
+            outputNames.append(returnOutputName(element))
+    else:
+        outputNames.append(returnOutputName(astNode.body[-1].value))
+    
+    outputTypes = []
+    if isinstance(astNode.returns, ast.Subscript):
+        for element in astNode.returns.slice.elts:
+            outputTypes.append(element.value.id + "." + element.attr if isinstance(element, ast.Attribute) else element.id)
+    else:
+        if isinstance(astNode.returns, ast.Name):
+            outputTypes.append(astNode.returns.id)
+        elif isinstance(astNode.returns, ast.Attribute):
+            outputTypes.append(astNode.returns.value.id + "." + astNode.returns.attr)
+    for outName, outType in zip(outputNames, outputTypes):
+        outputs[outName] = outType
+    return outputs
+
+            
+    
+    
+
+def getExpectedArgs(astNode):
+    args = {}
+    for arg in astNode.args.args:
+        if not arg.annotation:
+            print("No hint for expected input type. Please add it and try again")
+            return {}
+        args[arg.arg] = ast.unparse(arg.annotation).strip()
+    return args
+
+
+parser = argparse.ArgumentParser(description="Takes in a file of functions and returns a json object that describes them in the following format {'function_name': {'Description': text, 'Inputs': {'nameOfInput': 'typeofInput',...}, 'Outputs': {'nameOfOutput': 'typeofOutput',....}}, ...}")
+parser.add_argument("-f", "--targetFile", help = "Filepath to file containing functions")
+args = parser.parse_args()
+
+res = astUpdate(args.targetFile)
+print(res)
+with open("documentation.json", "w") as f:
+    json.dump(res, f)
+
